205de089e0f56b53993c2089a2992fcf79d44dbc
Ajay KC <meajay64@gmail.com>
2022-03-25 16:29:15 +0900

    Added One to Few Relationship

---
+++ user.js
@@ -0,0 +1,58 @@
+//One to Few Relationships Demo...
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/relationshipDemo");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const userSchema = new mongoose.Schema({
+  first: String,
+  last: String,
+  addresses: [
+    {
+      _id: { id: false },
+      street: String,
+      city: String,
+      state: String,
+      country: String,
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+const makeUser = async () => {
+  const u = new User({
+    first: "Harry",
+    last: "Potter",
+  });
+  u.addresses.push({
+    street: "123 Sesame St.",
+    city: "New York",
+    state: "NY",
+    country: "USA",
+  });
+  const res = await u.save();
+  console.log(res);
+};
+
+const addAddress = async (id) => {
+  const user = await User.findById(id);
+  user.addresses.push({
+    street: "99 3rd St.",
+    city: "New York",
+    state: "NY",
+    country: "USA",
+  });
+  const res = await user.save();
+  console.log(res);
+};
+
+addAddress("623cfe84a56c38b8279551b4");
+
+// makeUser();
